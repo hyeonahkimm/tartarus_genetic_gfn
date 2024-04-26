aeb4c7416a637391c96384cde23fd3e9a070c0f9
hyeonah kim <gusk6415@gmail.com>
2024-04-26 14:37:27 +0900

    zip

--- genetic_gfn/scoring_function.py
+++ genetic_gfn/scoring_function.py
@@ -3,6 +3,7 @@ import glob
 import numpy as np
 # from tdc import Oracle, Evaluator
 
+from tqdm import tqdm
 from rdkit.Chem import MolFromSmiles
 from rdkit import RDLogger
 RDLogger.DisableLog('rdApp.*')
@@ -21,35 +22,23 @@ DOCKING_INVALID_SCORE = 10**4
 #     evaluator = Evaluator(name = 'Diversity')
 #     return evaluator(smiles)
 
-# def get_scores(smiles, mode="QED"):
-#     smiles_groups = []
-#     group_size = len(smiles) / n_process
-#     for i in range(n_process):
-#         smiles_groups += [smiles[int(i * group_size):int((i + 1) * group_size)]]
-
-#     temp_data = []
-#     # pool = multiprocessing.Pool(processes = n_process)
-#     # for index in range(n_process):
-#     #     temp_data.append(pool.apply_async(get_scores_subproc, args=(smiles_groups[index], mode, )))
-#     # pool.close()
-#     # pool.join()
-#     # scores = []
-#     # for index in range(n_process):
-#     #     scores += temp_data[index].get()
-#     scores = get_scores_subproc(smiles_groups[0], mode)
-
-#     for filename in glob.glob("docking/mols/*"):
-#         if os.path.exists(filename):
-#             os.remove(filename)
+# def get_scores(smiles, mode="tadf_osc"):
+#     scores = get_scores_subproc(smiles, mode)
 
 #     return scores
 
 
 def get_scores(smiles, mode="tadf_osc", n_process=16):
+    if mode.startswith('tadf'):
+        scores = get_scores_subproc(smiles, mode)
+        return scores
+
     smiles_groups = []
     group_size = len(smiles) / n_process
     for i in range(n_process):
         smiles_groups += [smiles[int(i * group_size):int((i + 1) * group_size)]]
+
+    scores = get_scores_subproc(smiles_groups[0], mode)
 
     temp_data = []
     pool = multiprocessing.Pool(processes = n_process)
@@ -58,6 +47,7 @@ def get_scores(smiles, mode="tadf_osc", n_process=16):
     pool.close()
     pool.join()
     scores = []
+    
     for index in range(n_process):
         scores += temp_data[index].get()
 
@@ -78,10 +68,13 @@ def get_scores_subproc(smiles, mode):
                 scores += [-1.0, -1.0, -1.0, -1.0]
 
     elif mode == "tadf_osc":
-        for i in range(len(smiles)):
+        pbar = tqdm(range(len(smiles)))
+        for i in pbar:
             if mols[i] != None:
                 st, osc, combined = tadf.get_properties(smiles[i])
                 scores += [sigmoid_transformation(osc, mode), st, osc, combined]
+                # tqdm.write(f'{smiles[i]}: {scores[-4:]}')
+                pbar.set_postfix({'score': scores[-4], 'osc': osc})
             else:
                 scores += [-1.0, -1.0, -1.0, -1.0]
     
@@ -89,7 +82,6 @@ def get_scores_subproc(smiles, mode):
         for i in range(len(smiles)):
             if mols[i] != None:
                 st, osc, combined = tadf.get_properties(smiles[i])
-                import pdb; pdb.set_trace()
                 scores += [sigmoid_transformation(combined, mode), st, osc, combined]
             else:
                 scores += [-1.0, -1.0, -1.0, -1.0]
@@ -102,59 +94,6 @@ def get_scores_subproc(smiles, mode):
                 scores += [sigmoid_transformation(unnormalized, mode), unnormalized]
             else:
                 scores += [-1.0, -1.0]
-
-    elif mode == "docking_PLPro_7JIR_mpo":
-        for i in range(len(smiles)):
-            if mols[i] != None:
-                # docking_score = docking(smiles[i], receptor_file="data/targets/7jir+w2.pdbqt", box_center=[51.51, 32.16, -0.55])
-                docking_score, unnormalized = docking(smiles[i], receptor_file="data/targets/7jir+w2.pdbqt", box_center=[51.51, 32.16, -0.55], return_raw=True)
-                qed = oracle_QED(smiles[i])
-                sa = oracle_SA(smiles[i])  #(10 - oracle_SA(smiles[i])) / 9
-                scores += [0.8 * docking_score + 0.1 * qed + 0.1 * (10 - sa) / 9, unnormalized, qed, sa]
-                try:
-                    ligand_mol_file = f"./docking/tmp/mol_{smiles[i]}.mol"
-                    ligand_pdbqt_file = f"./docking/tmp/mol_{smiles[i]}.pdbqt"
-                    docking_pdbqt_file = f"./docking/tmp/dock_{smiles[i]}.pdbqt"
-                    for filename in [ligand_mol_file, ligand_pdbqt_file, docking_pdbqt_file]:
-                        if os.path.exists(filename):
-                            os.remove(filename)
-                except:
-                    pass
-                
-                if docking_score > 0.8:
-                    print(smiles[i], docking_score)
-            else:
-                scores += [-1.0, -1.0, 0.0, 0.0]
-
-    elif mode == "docking_RdRp":
-        for i in range(len(smiles)):
-            if mols[i] != None:
-                docking_score = docking(smiles[i], receptor_file="data/targets/RDRP.pdbqt", box_center=[93.88, 83.08, 97.29])
-                scores += [docking_score]
-            else:
-                scores += [-1.0]
-
-    elif mode == "docking_RdRp_mpo":
-        for i in range(len(smiles)):
-            if mols[i] != None:
-                docking_score, unnormalized = docking(smiles[i], receptor_file="data/targets/RDRP.pdbqt", box_center=[51.51, 32.16, -0.55], return_raw=True)
-                qed = oracle_QED(smiles[i])
-                sa = oracle_SA(smiles[i])  #(10 - oracle_SA(smiles[i])) / 9
-                scores += [0.8 * docking_score + 0.1 * qed + 0.1 * (10 - sa) / 9, unnormalized, qed, sa]
-                try:
-                    ligand_mol_file = f"./docking/tmp/mol_{smiles[i]}.mol"
-                    ligand_pdbqt_file = f"./docking/tmp/mol_{smiles[i]}.pdbqt"
-                    docking_pdbqt_file = f"./docking/tmp/dock_{smiles[i]}.pdbqt"
-                    for filename in [ligand_mol_file, ligand_pdbqt_file, docking_pdbqt_file]:
-                        if os.path.exists(filename):
-                            os.remove(filename)
-                except:
-                    pass
-                
-                if docking_score > 0.8:
-                    print(smiles[i], docking_score)
-            else:
-                scores += [-1.0, -1.0, 0.0, 0.0]
 
     else:
         raise Exception("Scoring function undefined!")
